304d92c7662147e67ac68ac20128f8728b358fd2
Brian Foody <brianfoody@gmail.com>
2023-12-11 22:29:01 +0000

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,94 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+import React, { useState, useEffect } from "react";
+
+const MedicationSchedule = () => {
+  const [schedule, setSchedule] = useState({});
+  const [checkedMeds, setCheckedMeds] = useState({});
+
+  useEffect(() => {
+    const csvData = `Medication,07:00,07:15,07:30,09:15,09:30,11:15,12:15,13:15,15:15,17:15,17:30,19:15,21:15,21:30,23:15,23:30
+    Dexafree,X,,,,,,X,,,,X,,,,,X
+    Hylo Forte,,X,,X,,X,,X,X,X,,X,X,,X,
+    Chloromycetin,,,,,X,,,,,,,,,X,,
+    Timofluid,,,X,,,,,,,,,,,,,`;
+
+    // Parsing and organizing the data
+    const lines = csvData.split("\n");
+    const headers = lines[0].split(",").slice(1);
+    const tempSchedule = {};
+
+    lines.slice(1).forEach((line) => {
+      const parts = line.split(",");
+      const medication = parts[0];
+      parts.slice(1).forEach((part, index) => {
+        if (part === "X") {
+          const time = headers[index];
+          if (!tempSchedule[time]) {
+            tempSchedule[time] = [];
+          }
+          tempSchedule[time].push(medication);
+        }
+      });
+    });
+
+    setSchedule(tempSchedule);
+    loadCheckedMedications();
+  }, []);
+
+  const toggleGreen = (time) => {
+    setCheckedMeds((prev) => {
+      const newMeds = { ...prev };
+      newMeds[time] = !newMeds[time];
+      saveCheckedMedications(time, newMeds[time]);
+      return newMeds;
+    });
+  };
+
+  const saveCheckedMedications = (time, isChecked) => {
+    const today = new Date().toDateString();
+    const meds = JSON.parse(localStorage.getItem(today) || "{}");
+    meds[time] = isChecked;
+    localStorage.setItem(today, JSON.stringify(meds));
+  };
+
+  const loadCheckedMedications = () => {
+    const today = new Date().toDateString();
+    const meds = JSON.parse(localStorage.getItem(today) || "{}");
+    setCheckedMeds(meds);
+  };
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        {Object.keys(schedule)
+          .sort()
+          .map((time) => (
+            <button
+              key={time}
+              className={`${styles.button} ${
+                checkedMeds[time] ? styles.green : ""
+              }`}
+              onClick={() => toggleGreen(time)}
+            >
+              {`${time}: ${schedule[time].join(", ")}`}
+            </button>
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Medications</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
+      <MedicationSchedule />
 
       <style jsx>{`
         main {
@@ -92,15 +122,8 @@ export default function Home() {
           border-radius: 5px;
           padding: 0.75rem;
           font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
       `}</style>
 
@@ -109,17 +132,8 @@ export default function Home() {
         body {
           padding: 0;
           margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
         * {
